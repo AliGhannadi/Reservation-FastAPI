cf0c5dfd82640001714cda0de7d1c6248514a154
Ali Ghannadi <ali.ghannadi801@gmail.com>
2025-10-09 15:33:25 +0330

    Added changing username, password, email, firstname, lastname + Bug Fixes

--- routers/users.py
+++ routers/users.py
@@ -69,4 +69,69 @@ async def create_user(db: db_dependency, create_user_request: CreateUser):
 async def get_all_users(db: db_dependency):
     user_model = db.query(Users).all()
     return user_model
+
+@router.get("/{user_id}")
+async def get_user_by_id(user_id: int, db: db_dependency):
+    user_model = db.query(Users).filter(Users.id == user_id).first()
+    if not user_model:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user_model
+
+@router.put("/change_first_name/{user_id}")
+async def change_first_name(user_id: int, new_first_name: str, db: db_dependency):
+    user_model = db.query(Users).filter(Users.id == user_id).first()
+    if not user_model:
+        raise HTTPException(status_code=404, detail="User not found")
+    user_model.first_name = new_first_name
+    db.commit()
+    db.refresh(user_model)
+    return user_model
+
+@router.put("/change_last_name/{user_id}")
+async def change_last_name(user_id: int, new_last_name: str, db: db_dependency):
+    user_model = db.query(Users).filter(Users.id == user_id).first()
+    if not user_model:
+     raise HTTPException(status_code=404, detail="User not found")
+    user_model.last_name = new_last_name
+    
+@router.put("/change_phone_number/{user_id}")
+async def change_phone_number(user_id: int, new_phone_number: str, db: db_dependency):
+    user_model = db.query(Users).filter(Users.id == user_id).first()
+    if not user_model:
+        raise HTTPException(status_code=404, detail="User not found")
+    user_model.phone_number = new_phone_number
+    db.commit()
+    db.refresh(user_model)
+    return user_model
+
+@router.put("/change_email/{user_id}")
+async def change_email(user_id: int, new_email: str, db: db_dependency):
+    user_model = db.query(Users).filter(Users.id == user_id).first()
+    if not user_model:
+        raise HTTPException(status_code=404, detail="User not found")
+    user_model.email = new_email
+    db.commit()
+    db.refresh(user_model)
+    return user_model
+@router.put("/change_password/{user_id}")
+async def change_password(user_id: int, new_password: str, db: db_dependency):
+    user_model = db.query(Users).filter(Users.id == user_id).first()
+    if not user_model:
+        raise HTTPException(status_code=404, detail="User not found")
+    user_model.hashed_password = bcrypt_context.hash(new_password[:72])
+    db.commit()
+    db.refresh(user_model)
+    return user_model
+
+@router.put('/change_username/{user_id}')
+async def change_username(user_id: int, new_username: str, db: db_dependency):
+    user_model = db.query(Users).filter(Users.id == user_id).first()
+    if not user_model:
+        raise HTTPException(status_code=404, detail="User not found")
+    user_model.username = new_username
+    db.commit()
+    db.refresh(user_model)
+    return user_model
+
+
     
